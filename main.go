739df8553bbde9b965b8502048a1868b92e29075
jessie-sr <jessie_sr@163.com>
2024-11-05 21:13:48 -0500

    Get the server running

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Port is not found in the environment")
 	}
 	fmt.Println("Port:", portString)
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }

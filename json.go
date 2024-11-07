719f5e1da03ce35fc40350d17fab6ef8ecb2a2fd
jessie-sr <jessie_sr@163.com>
2024-11-07 15:50:24 -0500

    Created json.go

---
+++ json.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload) // Returns the JSON encoding of payload in bytes
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v", payload)
+		w.WriteHeader(500) // Internal server error
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(200) // Success
+	w.Write(data)      // Respond with data body
+}

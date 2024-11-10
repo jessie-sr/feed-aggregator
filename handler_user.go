20d1e2412ee169297e8db6523187ac91115022f6
jessie-sr <jessie_sr@163.com>
2024-11-10 15:41:40 -0500

    Improved comments

--- handler_user.go
+++ handler_user.go
@@ -15,15 +15,16 @@ func (apiCig *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request
 	type parameter struct {
 		Name string `json:"name"`
 	}
-	decoder := json.NewDecoder(r.Body) // Creates a JSON decoder that reads from the request body
+	decoder := json.NewDecoder(r.Body) // Create a JSON decoder that reads from the request body
 
 	params := parameter{}
-	err := decoder.Decode(&params) // Reads from the request body into the params struct
+	err := decoder.Decode(&params) // Read from the request body into the params struct
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintln("Error parsing JSON:", err))
 		return
 	}
 
+	// Create a new user using db.CreateUserParams
 	user, err := apiCig.DB.CreateUser(r.Context(), db.CreateUserParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
@@ -35,16 +36,19 @@ func (apiCig *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Return the user through our custom user model
 	respondWithJSON(w, 201, dbUserToUser(user))
 }
 
 func (apiCig *apiConfig) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	// Get API key from the request header
 	apikey, err := auth.GetApiKey(r.Header)
 	if err != nil {
 		respondWithError(w, 403, fmt.Sprintf("Authentication error: %v", err))
 		return
 	}
 
+	// Get user with the API key
 	user, err := apiCig.DB.GetUserByApiKey(r.Context(), apikey)
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Error getting user: %s", err))

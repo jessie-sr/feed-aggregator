02e9cf409c091329bfac5e422ea73cba4479f260
jessie-sr <jessie_sr@163.com>
2024-11-16 12:01:23 -0500

    Fixed naming confusion

--- scraper.go
+++ scraper.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/jessie-sr/rss-aggregator/internal/db"
 )
 
-func startScraping(db *db.Queries,
+func startScraping(DB *db.Queries,
 	concurrency int, // # of different goroutines to run for the scraping process.
 	timeBetweenRequest time.Duration, // Time interval between scraping requests
 ) {
 	log.Printf("Scraping on %v Goroutines every %v durations", concurrency, timeBetweenRequest)
 	ticker := time.NewTicker(timeBetweenRequest)
 	for ; ; <-ticker.C { // Block until the ticker sends a value on its ticker.C (happens at every timeBetweenRequest interval)
-		feeds, err := db.GetNextFeedsToFetch(context.Background(), int32(concurrency))
+		feeds, err := DB.GetNextFeedsToFetch(context.Background(), int32(concurrency))
 		if err != nil {
 			log.Println("Error fetching feeds: ", err)
 			continue // Should always be running as our server operates
@@ -26,16 +28,16 @@ func startScraping(db *db.Queries,
 		for _, feed := range feeds {
 			wg.Add(1)
 
-			go scrapeFeed(db, wg, feed) // Concurrent scraping
+			go scrapeFeed(DB, wg, feed) // Concurrent scraping
 		}
 		wg.Wait() // Wait for {concurrency} number of goroutines to finish their scraping
 	}
 }
 
-func scrapeFeed(db *db.Queries, wg *sync.WaitGroup, feed db.Feed) {
+func scrapeFeed(DB *db.Queries, wg *sync.WaitGroup, feed db.Feed) {
 	defer wg.Done()
 
-	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
+	_, err := DB.MarkFeedAsFetched(context.Background(), feed.ID)
 	if err != nil {
 		log.Println("Error marking feed as fetched: ", err)
 		return
@@ -47,11 +49,39 @@ func scrapeFeed(db *db.Queries, wg *sync.WaitGroup, feed db.Feed) {
 		return
 	}
 
-	// This log is for testing purpose
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds) // Use precise timestamps to avoid logs being out of order due to concurrency or buffer issues
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		// log.Println("Found post", item.Title, "on feed", feed.Name) // This log is for testing purpose
+
+		// Initialize description
+		description := sql.NullString{}
+
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		// Parse time
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing date %v with error %v: ", item.PubDate, err)
+			continue
+		}
+
+		_, err = DB.CreatePost(context.Background(), db.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			log.Println("Error creating post: ", err)
+		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 
